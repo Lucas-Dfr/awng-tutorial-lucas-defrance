9547f37b27b8dbba59ba6d44f215f871b66b72af
Lucas Defrance <lucas.defrance@student.isae-supaero.fr>
2022-04-12 09:02:53 +0200

    Importing get_object_or_404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
